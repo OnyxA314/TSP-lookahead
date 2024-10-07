d1f4bd5d7b770ccf0388326f7e9c314eacddd463
Onyx_A314 <onyxa314@gmail.com>
2024-10-07 16:07:11 -0500

    added code to show what node currently on and what the nodes contain the shortest path from the current node on

--- file_distance_calc.cpp
+++ file_distance_calc.cpp
@@ -18,8 +18,10 @@ int main (void)
 
 	int total_points;
 
+	int node_tracker = 0; //keep track what node currently on
+	
+	char filler; //filler to make program temp pause until user presses enter
 
-	//TODO: ask for userinput on what file to use. user enters number, such as 2, and then the program uses point_file_x.txt with x being the userinput. allows easier testing
 	ifstream point_file ("point_file.txt");
 
 	
@@ -43,23 +45,73 @@ int main (void)
 	}
 	cout << "\n\n\n"; //code to seperate making sure we got the points to the actual math
 
+	
 
+	//TODO: we can make this even faster. we can only check x and y points and see if those are greater than 3 shortest already known.
+	//	if they x and y points are greater we know the distance is greater so we don't have to waste time doing the math to already figure out what we know.
+	//	i.e. at point (0,0) we know point (5,1) is going to be closer than (6, 2) because the x and y is greater both greater. 
+	
 	//TODO: this can be simplified. right now it calculates every point when we can simplify this. we are assuming x -> y is the same as y -> x. we can create code to prevent recalculation
 	for (auto cord : cords) //for every cordinate we have
 	{
 		double xinit = cord.first;	//initially set the x cordinate to current x cordinate we are at
 		double yinit = cord.second;	//initially set the y cordinate to current y cordinate we are at
+		
+		//creates 3 shortest variables and sets them to virtual infinity for testing
+		float first_shortest, second_shortest, third_shortest;
+		first_shortest = 999999999;
+		second_shortest = 999999999;
+		third_shortest = 999999999;
+
+		float first_shortest_node, second_shortest_node, third_shortest_node;
+		int inner_node_tracker = 0;	
+		
+		cout << "Current Node: " << node_tracker << endl;
 
 		for (auto cord2 : cords)
 		{
+			
 			double x2 = cord2.first;	//gets new cordinates for every point
 			double y2 = cord2.second;
 
 			double distance = sqrt(pow(x2 - xinit, 2) + pow(y2 - yinit, 2));	//calculates distance from initial point to every other point
-
-			cout << distance << endl;	//prints out the distance from every point
-		}
 		
+			
+			if(xinit == x2 && yinit == y2)
+			{
+				//this is testing to see if we are on the point we are currently measuing. if we are skip testing to see if it's shorter as the distance from itself is 0
+			}			
+			else if (distance < first_shortest)
+			{
+				first_shortest = distance;
+				first_shortest_node = inner_node_tracker;
+			}
+			else if (distance < second_shortest)
+			{
+				second_shortest = distance;
+				second_shortest_node = inner_node_tracker;
+			}
+			else if (distance < third_shortest)
+			{
+				third_shortest = distance;
+				third_shortest_node = inner_node_tracker;
+			}
+
+
+			cout << "Node " << node_tracker << " to " << inner_node_tracker << " has distance: " << distance << endl;	//prints out the distance from every point
+			inner_node_tracker++;
+			
+		}
+
+		cout << "\nTHE SHORTEST DISTANCES FROM NODE " << node_tracker <<":\n";
+		cout << "Node " << first_shortest_node << " with distance " << first_shortest << endl;
+		cout << "Node " << second_shortest_node << " with distance " << second_shortest << endl;
+		cout << "Node " << third_shortest_node << " with distance " << third_shortest << endl;
+		//cout << "Press enter to continue";
+		//cin >> filler;
+
+		node_tracker++;
+
 		cout << endl << endl;
 	}
 
