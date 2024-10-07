6e354af7606eecc9e07b8be8f8c76158eccd9095
Onyx_A314 <onyxa314@gmail.com>
2024-10-07 13:19:46 -0500

    the start of calculating distance of x points from a file

---
+++ file_distance_calc.cpp
@@ -0,0 +1,56 @@
+//like distance calc but gets x distances from a file, then calcualtes how far each is
+//file should have first line say how many points in the file, then each line after that has 2 numbers seperated by a space. first number is xcord of the point, second number is the ycord of the point
+
+
+#include <iostream>
+#include <fstream>
+#include <cmath> 
+#include <vector> //Vectors my beloved
+#include <utility> //for ::pair to store 2D cords. first time using this
+
+
+using namespace std;
+
+int main (void)
+{
+	vector<pair<double, double>> cords; //creates a vector of pairs called cords of the form (double, double) 
+	double x, y;	
+
+	int total_points;
+
+
+	//TODO: ask for userinput on what file to use. user enters number, such as 2, and then the program uses point_file_x.txt with x being the userinput. allows easier testing
+	ifstream point_file ("point_file_1.txt");
+
+	
+	point_file >> total_points; //gets total points from the first line in the input file
+	
+	cout << total_points << endl;
+
+
+	for (int i = 0; i < total_points; i++) //as long as there are points to be calculated (gotten from first line of .txt file does the code)
+	{
+		//gets the x, y points for each point
+		point_file >> x;
+		point_file >> y;
+
+		cords.push_back(make_pair(x, y)); //adds the cordinate pair to the vector 'cords'
+	}
+	
+	for (auto cord : cords) //prints out every single cordinate gotten from file. really just a trouble shooting step to make sure everything works
+	{
+		cout << "Cordinate: " << cord.first << ", " << cord.second << "\n";
+	}
+
+	return 0;
+
+
+
+	//TODO: Go through each point and calculate the distance from every other point
+
+	for (int i = 0; i < total_points; i++)
+	{
+
+
+	
+}
